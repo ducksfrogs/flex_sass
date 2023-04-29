46033313988ebc3b3d6ed13941fc81c12887e4f4
ducksfrogs <ma_yamaki@yahoo.com>
2023-04-30 07:25:00 +0900

    dd

--- packs/test/src/js/main.js
+++ packs/test/src/js/main.js
@@ -3,7 +3,12 @@ import "../style/styles.scss";
 console.log('hello webpack');
 
 import img from '../img/img1.jpg';
+import img2 from '../img/img2.jpg'
 
 const imgJpg = document.createElement('img');
 imgJpg.src = img;
 document.getElementById('box').appendChild(imgJpg);
+
+const img2Jpg = document.createElement('img');
+img2Jpg.src = img2;
+document.getElementById('Img2').appendChild(img2Jpg);
