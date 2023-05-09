3dad935c5c6a4814c588deb031c74a0fa3d77bc3
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-10 05:50:43 +0900

    dd

--- lessons/responsive/src/js/main.js
+++ lessons/responsive/src/js/main.js
@@ -3,8 +3,13 @@ import "../style/styles.scss";
 console.log('hello webpack') ;
 
 import img1 from '../img/img1.jpg';
+import img2 from '../img/ducklings.jpg';
 
 
 const imgOne = document.createElement('img');
 imgOne.src = img1;
 document.getElementById('test-i').appendChild(imgOne);
+
+const imgDuck = document.createElement('img');
+imgDuck.src = img2;
+document.getElementById('duck').appendChild(imgDuck);
