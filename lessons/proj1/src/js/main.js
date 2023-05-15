a828dc0eaca6887c9ee9331f159dd0afd59e94cf
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-16 07:01:20 +0900

    d

--- lessons/proj1/src/js/main.js
+++ lessons/proj1/src/js/main.js
@@ -7,3 +7,12 @@ import img1 from '../img/img1.jpg';
 const hCornerI = document.createElement('img');
 hCornerI.src = img1;
 document.getElementById('header-corner__img').appendChild(hCornerI);
+
+
+const hamburberContainer = document.querySelector('#header__main-nav');
+const hamburber = document.querySelector('.header__main-nav--hamburger');
+const links = document.querySelectorAll('.header__main-nav--links li');
+
+hamburber.addEventListener('click', () => {
+    hamburberContainer.classList.toggle("clicked");
+});
