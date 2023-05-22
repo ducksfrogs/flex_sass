5f96df7dc7ee7b0a95290f9a034f0d59d7666322
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-23 06:01:03 +0900

    d

--- lessons/cssTest/webpack.config.js
+++ lessons/cssTest/webpack.config.js
@@ -4,7 +4,19 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader', 
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+        ],
     },
 };
