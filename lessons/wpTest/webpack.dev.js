147064d814b53e2b9b98ad56d6975fe51db1ca4a
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-23 22:13:46 +0900

    dddd

--- lessons/wpTest/webpack.dev.js
+++ lessons/wpTest/webpack.dev.js
@@ -1,27 +1,10 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+module.exports = merge(common{
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
     },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader, 
-                    'css-loader',
-                    'sass-loader'
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: './css/[name].css',
-        }),
-    ],
-};
+});
--- lessons/wpTest/webpack.prod.js
+++ lessons/wpTest/webpack.prod.js
@@ -1,27 +1,7 @@
-const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader, 
-                    'css-loader',
-                    'sass-loader'
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: './css/[name].css',
-        }),
-    ],
-};
+
+module.exports = merge(common, {
+    mode: 'production',
+})
