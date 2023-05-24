f1cee6d3e1aebd1cd188fb1177ba0d04794f18f3
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-24 18:29:46 +0900

    webpack prod mode

--- lessons/proj1_test/webpack.prod.js
+++ lessons/proj1_test/webpack.prod.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
 const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 
@@ -7,4 +9,21 @@ module.exports = merge( common, {
         filename: './main.js',
         path: path.resolve(__dirname, 'prod'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: './css/[name].css',
+        }),
+    ],
 });
