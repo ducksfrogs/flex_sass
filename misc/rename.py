d500733d590263bc016422f7f0208777f956751c
ducksfrogs <ma_yamaki@yahoo.com>
2023-06-19 07:14:52 +0900

    change python file

--- misc/rename.py
+++ misc/rename.py
@@ -2,14 +2,14 @@ import glob
 import os
 path = 'img/*.jpg'
 i = 1
-
+fname = input("dd :")
 
 flist = glob.glob(path)
 print("before")
 print(flist)
 
 for file in flist:
-    os.rename(file, './img/img' + str(i) + '.jpg')
+    os.rename(file, './img/' + fname + str(i) + '.jpg')
     i += 1
 
 list = glob.glob(path)
--- misc/resize_up.py
+++ misc/resize_up.py
@@ -3,11 +3,12 @@ from PIL import Image
 import os
 import glob
 
+FName = input("Enter file name: ")
 
-dst_dir = 'data/images_half'
+dst_dir = 'data/temp/images_half'
 os.makedirs(dst_dir, exist_ok=True)
 
-files = glob.glob('./data/*.jpg')
+files = glob.glob('./data/img/*.jpg')
 
 for f in files:
     img = Image.open(f)
@@ -15,3 +16,6 @@ for f in files:
     root, ext =os.path.splitext(f)
     basename = os.path.basename(root)
     img_resize.save(os.path.join(dst_dir, basename + '_half' + ext))
+
+    os.rename(file, './img/img' + str(i) + '.jpg')
+    i += 1
