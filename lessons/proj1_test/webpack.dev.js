050e322fdee684442508b0749f853cf4fa8135bb
ducksfrogs <ma_yamaki@yahoo.com>
2023-06-20 18:36:07 +0900

    fix some

--- lessons/proj1_test/webpack.dev.js
+++ lessons/proj1_test/webpack.dev.js
@@ -3,12 +3,8 @@ const { merge } = require('webpack-merge');
 
 module.exports = merge( common, {
     mode: 'development',
-    output: {
-        filename: './main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
     devServer: {
-        static: path.resolve(__dirname, 'dist'),
+        static: './dist',
         port: 8081,
         hot: true
     },
